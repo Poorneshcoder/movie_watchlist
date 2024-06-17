3523a0aa667083a158ff08b6e004071d3dce46f8
Poornesh Pithani <102178667+Poorneshcoder@users.noreply.github.com>
2024-06-17 19:16:31 +0530

    Update movieActions.js

--- client/src/actions/movieActions.js
+++ client/src/actions/movieActions.js
@@ -28,13 +28,6 @@ export const addMovie = (movie) => async (dispatch) => {
   }
 };
 
-// Example call to add a movie
-addMovie({
-  title: 'Inception',
-  director: 'Christopher Nolan',
-  year: 2010
-});
-
 // Update movie
 export const updateMovie = (id, updatedMovie) => async (dispatch) => {
   try {
